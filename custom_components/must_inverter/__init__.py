361bb3d073878515750d7fb6dcbd857c04221b03
Muka Schultze <samuelschultze@gmail.com>
2024-11-01 21:23:40 -0300

    feat: use `async_forward_entry_setups` for loading platforms

--- custom_components/must_inverter/__init__.py
+++ custom_components/must_inverter/__init__.py
@@ -38,10 +38,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
         hass.data[DOMAIN][entry.entry_id] = inverter
 
-        for component in PLATFORMS:
-            hass.async_create_task(
-                hass.config_entries.async_forward_entry_setup(entry, component)
-            )
+        await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
         return True
     except ConfigEntryNotReady as ex:
@@ -51,15 +48,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
         raise ConfigEntryNotReady(f"Unknown error connecting to modbus device") from ex
 
 async def async_unload_entry(hass, entry):
-    """Unload a config entry."""
-    unload_ok = all(
-        await asyncio.gather(
-            *[
-                hass.config_entries.async_forward_entry_unload(entry, component)
-                for component in PLATFORMS
-            ]
-        )
-    )
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
     if not unload_ok:
         return False
