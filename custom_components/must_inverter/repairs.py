9b5922b2f6852fdc6c51c4e4945ec41b8c887d4a
Muka Schultze <samuelschultze@gmail.com>
2025-02-12 21:20:38 +0000

    fix: serial number repair update existing entities instead of creating new ones

--- custom_components/must_inverter/repairs.py
+++ custom_components/must_inverter/repairs.py
@@ -1,14 +1,19 @@
 from __future__ import annotations
 
+import logging
 import voluptuous as vol
 import time
 
 from homeassistant import data_entry_flow
 from homeassistant.components.repairs import RepairsFlow
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import device_registry as dr
+from homeassistant.helpers import entity_registry as er
 
 from .__init__ import MustInverter
 from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class NoSerialNumber(RepairsFlow):
@@ -28,10 +33,33 @@ class NoSerialNumber(RepairsFlow):
         if user_input is not None:
             entry_id = self._data["entry_id"]
             device: MustInverter = self._hass.data[DOMAIN][entry_id]["inverter"]
+
+            old_serial_number = device.data["InverterSerialNumber"]
             new_serial_number = user_input["new_serial"]
 
             lower = new_serial_number & 0xFFFF
             upper = (new_serial_number >> 16) & 0xFFFF
+            dr_entries = dr.async_entries_for_config_entry(dr.async_get(self._hass), entry_id)
+
+            for device_entry in dr_entries:
+                try:
+                    _LOGGER.info("updating device identifiers to %s", new_serial_number)
+                    dr.async_get(self._hass).async_update_device(
+                        device_entry.id, new_identifiers={(DOMAIN, new_serial_number)}
+                    )
+                except Exception as err:
+                    _LOGGER.error("failed to update device identifiers: %s", err)
+
+            er_entries = er.async_entries_for_config_entry(er.async_get(self._hass), entry_id)
+
+            for entity_entry in er_entries:
+                try:
+                    old_id = entity_entry.unique_id
+                    new_id = old_id.replace(str(old_serial_number), str(new_serial_number))
+                    _LOGGER.info("updating entity id from %s to %s", old_id, new_id)
+                    er.async_get(self._hass).async_update_entity(entity_entry.entity_id, new_unique_id=new_id)
+                except Exception as err:
+                    _LOGGER.error("failed to update entity id: %s", err)
 
             await device.write_modbus_data(20002, upper)
             await device.write_modbus_data(20003, lower)
@@ -42,11 +70,7 @@ class NoSerialNumber(RepairsFlow):
 
         return self.async_show_form(
             step_id="confirm",
-            data_schema=vol.Schema(
-                {
-                    vol.Required("new_serial", default=int(time.time())): int,
-                }
-            ),
+            data_schema=vol.Schema({vol.Required("new_serial", default=int(time.time())): int}),
         )
 
 
